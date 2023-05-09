3308f082f05113d926a72231b2ce2fb579a14e23
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-05-09 17:23:17 +0300

    fix(03): repair code without sync

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,22 @@
-const fs = require('fs');
+const tool = require('node:fs/promises');
 const path = require('path');
-fs.readdir('./03-files-in-folder/secret-folder/', (err, data) => {
-    console.log(data);
-    data.forEach(file =>{
-        console.log(file+' - '+path.extname(file)+' - '+fs.statSync('./03-files-in-folder/secret-folder/'+file).size+'B');
-        console.log()
-    });
-});
+const fs = require('fs');
+const fullPath = path.join(__dirname);
+const getFiles = async () => {
+  try {
+    const files = await tool.readdir(fullPath + '/secret-folder');
+    for (const file of files) {
+      fs.stat(fullPath + '/secret-folder/' + file, (err, stats) => {
+        if (stats.isFile()) {
+          const fSize = stats.size;
+          const fName = file.substring(0, file.lastIndexOf('.'));
+          const fExt = file.substring(file.lastIndexOf('.') + 1);
+          console.log(`${fName}-${fExt}-${fSize}B`);
+        }
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+getFiles();
