98a01e9d8f7d6ca36d04cb9e069b7397b9d03756
mikhalenkodaniil <mikhalenko.daniil@gmail.com>
2023-05-09 06:20:08 +0300

    feat(04): add script copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 
+  fs.mkdir('./04-copy-directory/files-copy', (err) => {
+  });
+fs.readdir('./04-copy-directory/files/', (err, data) => {
+    data.forEach(file =>{
+        fs.copyFile('./04-copy-directory/files/'+file+'/', './04-copy-directory/files-copy/'+file+'/', (err) => {});
+    });
+});
